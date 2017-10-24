d67a7eb50a14e672b60a70608a6c9f4346a77785
lizhe <totti_912@sina.com>
2017-10-24 17:50:14 +0800

    增加动画插件

--- src/main.js
+++ src/main.js
@@ -4,12 +4,15 @@ import Vue from 'vue';
 import FastClick from 'fastclick';
 import router from './router';
 import App from './App';
+import AnimatedVue from 'animated-vue';
 import { ToastPlugin, AjaxPlugin } from 'vux';
 import { store } from './vuex/vuex';
+import '@/common/css/animate.css';
 import '@/common/stylus/index.styl';
 
 Vue.use(AjaxPlugin);
 Vue.use(ToastPlugin);
+Vue.use(AnimatedVue);
 
 FastClick.attach(document.body);
 
