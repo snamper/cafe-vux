bb5abc93a6cdaba882d5f00ea0a6ec5a32ae3872
philosophy <totti_912@sina.com>
2017-12-26 23:00:37 +0800

    已实现显示detail页面，但滚动失效，是否可以考虑用路由方式来显示

--- src/page/orderlist/detail.vue
+++ src/page/orderlist/detail.vue
@@ -1,33 +1,30 @@
 <template>
   <transition name="move">
-      <div v-show="showFlag" class="food" ref="food">
-        <x-header :title="food.name" :left-options="{showBack: true, preventGoBack: true}" @on-click-back="back"></x-header>
-        <div class="food-content">
-          <div class="image-header">
-            <img :src="food.imageUrl">
-          </div>
-          <div class="content">
-            <!-- <h1 class="title">{{food.name}}</h1> -->
-            <!-- <div class="detail">
-              <span class="sell-count">月售{{food.sellCount}}份</span>
-              <span class="rating">好评率{{food.rating}}%</span>
-            </div> -->
-            <div class="price">
-              <span>会员价</span><span class="now">￥{{food.memberPrice}}</span><span>非会员价</span><span class="old" v-show="food.price">￥{{food.price}}</span>
-            </div>
-            <div class="cartcontrol-wrapper">
-              <cartcontrol @add="addFood" :food="food"></cartcontrol>
-            </div>
-            <transition name="fade">
-              <div @click.stop.prevent="addFirst" class="buy" v-show="!food.count || food.count===0">
-                加入购物车
-              </div>
-            </transition>
-          </div>
-          <split v-show="food.info"></split>
-          <div class="info" v-show="food.description">
-            <h1 class="title">简介</h1>
-            <p class="text">{{food.description}}</p>
+      <div v-show="showFlag" class="food" ref="food" v-if="list">
+        <x-header :title="list.code" :left-options="{showBack: true, preventGoBack: true}" @on-click-back="back"></x-header>
+        <div class="detail-desciption">
+          <span class="title">结算时间</span>
+          <span class="time">{{buyDateTime(list.createTime)}}</span>
+        </div>
+        <divider>详情</divider>
+        <div class="detail-wrapper">
+          <ul>
+            <li v-for="(good,index) in list.details" :key="index">
+                <div class="detail">
+                    <div class="avator">
+                        <avator :img="good.imageUrl" size='35' radius='50'></avator>
+                    </div>
+                    <div class="content">
+                        <div class="name">{{good.productName}}</div>
+                        <div class="number">x{{good.number}}</div>
+                        <div class="total">￥{{good.amount}}</div>
+                    </div>
+                </div>
+            </li>
+          </ul>
+          <split></split>
+          <div class="summary">
+              <div class="title">总价</div><div class="price">￥{{list.amount}}</div>
           </div>
         </div>
       </div>
@@ -37,7 +34,9 @@
 <script type="text/ecmascript-6">
 // import BScroll from 'better-scroll';
 import split from '../../components/split/split';
-import { XHeader } from 'vux';
+import avator from '../../components/avator/avator';
+import { XHeader, Divider } from 'vux';
+import util from '../../common/js/util';
 import Logger from 'chivy';
 const log = new Logger('page/orderlist/detail');
 
@@ -65,11 +64,21 @@ export default {
         //     this.scroll.refresh();
         //   }
         // });
+      },
+      back() {
+        this.showFlag = false;
+      },
+      buyDateTime(date) {
+            let format = util.formatDate(date);
+            let result = format.Year + '-' + format.Month + '-' + format.Day + ' ' + format.Hour + ':' + format.Minute;
+            return result;
       }
     },
     components: {
       split,
-      XHeader
+      XHeader,
+      Divider,
+      avator
     }
 };
 </script>
@@ -90,87 +99,65 @@ export default {
       transition: all 0.2s linear
     &.move-enter, &.move-leave-active
       transform: translate3d(100%, 0, 0)
-    .image-header
+    .detail-desciption
+      height 30px
       display flex
-      justify-content center
       align-items center
+      .title,.time
+        font-size 16px
+        padding-top 5px
+        padding-bottom 5px
+      .title
+        flex-grow 1
+        padding-left 20px
+      .time
+        flex-grow 1
+        text-align right
+        padding-right 10px
+    .detail-wrapper
+      position absolute
       width 100%
-      img
-        width 70%
-        height 70%
-        padding 1rem 0
-      .back
-        position: absolute
-        top: 10px
-        left: 0
-        .icon-arrow_lift
-          display: block
-          padding: 10px
-          font-size: 20px
-          color: #fff
-
-    .content
-      position: relative
-      padding: 10px 18px
-      .title
-        line-height: 14px
-        margin-bottom: 8px
-        font-size: 14px
-        font-weight: 700
-        color: rgb(7, 17, 27)
+      top 108px
+      bottom 50px
+      overflow hidden
+      .time
+          padding 0.3rem 0
       .detail
-        margin-bottom: 18px
-        line-height: 10px
-        height: 10px
-        font-size: 0
-        .sell-count, .rating
-          font-size: 10px
-          color: rgb(147, 153, 159)
-        .sell-count
-          margin-right: 12px
-      .price
-        font-weight: 700
-        line-height: 24px
-        .now
-          margin-right: 8px
-          font-size: 14px
-          color: rgb(240, 20, 20)
-        .old
-          font-size: 14px
-          color: rgb(7, 17, 27)
-      .cartcontrol-wrapper
-        position: absolute
-        right: 12px
-        bottom: 6px
-      .buy
-        position: absolute
-        right: 18px
-        bottom: 10px
-        z-index: 10
-        height: 24px
-        line-height: 24px
-        padding: 0 12px
-        box-sizing: border-box
-        border-radius: 12px
-        font-size: 10px
-        color: #fff
-        background: rgb(0, 160, 220)
-        opacity: 1
-        &.fade-enter-active, &.fade-leave-active
-          transition: all 0.2s
-        &.fade-enter, &.fade-leave-active
-          opacity: 0
-          z-index: -1
-    .info
-      padding: 18px
-      .title
-        line-height: 14px
-        margin-bottom: 6px
-        font-size: 14px
-        color: rgb(7, 17, 27)
-      .text
-        line-height: 24px
-        padding: 0 8px
-        font-size: 12px
-        color: rgb(77, 85, 93)
+          width 100%
+          height 60px
+          display flex
+          justify-content center
+          align-items center
+          .avator, .content
+              display inline-block
+          .avator
+              flex-grow 1
+              text-align center
+          .content 
+              flex-grow 3
+              display flex
+              .name, .number,.total
+                  display inline-block
+                  width 100%
+              .name
+                  text-align left
+              .number
+                  text-align right
+              .total
+                  text-align center
+      .summary
+        width 100%
+        height 2rem
+        display flex
+        justify-content center
+        align-items center
+        .title,.price
+            width 100%
+            line-height 18px
+        .title
+            text-align left
+            padding-left 3rem 
+        .price
+            text-align right
+            padding-right 2rem
 </style>
