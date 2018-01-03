06249b95001337835b6834454f6706c6f2617563
philosophy <totti_912@sina.com>
2018-01-03 17:33:33 +0800

    增加deploy脚本便于部署

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+rm -r /home/shop/procs/ppus/web.ppu/framework-web-pmu.pmu/shop-web-container.par/web.ear/web.war/static
+rm -r /home/shop/procs/ppus/web.ppu/framework-web-pmu.pmu/shop-web-container.par/web.ear/web.war/index.html
+cp -R /home/font-end/cafe-vux/dist/static /home/shop/procs/ppus/web.ppu/framework-web-pmu.pmu/shop-web-container.par/web.ear/web.war
+cp /home/font-end/cafe-vux/dist/index.html /home/shop/procs/ppus/web.ppu/framework-web-pmu.pmu/shop-web-container.par/web.ear/web.war
