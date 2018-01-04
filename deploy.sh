fc5054dc0b41154d78d0e0ce4c97aaa775b2a307
philosophy <totti_912@sina.com>
2018-01-04 17:20:39 +0800

    加入自动下载代码以及编译拷贝的脚本

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
+cd /home/font-end/cafe-vux
+git pull origin router
+cnpm run build
 rm -r /home/shop/procs/ppus/web.ppu/framework-web-pmu.pmu/shop-web-container.par/web.ear/web.war/static
 rm -r /home/shop/procs/ppus/web.ppu/framework-web-pmu.pmu/shop-web-container.par/web.ear/web.war/index.html
 cp -R /home/font-end/cafe-vux/dist/static /home/shop/procs/ppus/web.ppu/framework-web-pmu.pmu/shop-web-container.par/web.ear/web.war
