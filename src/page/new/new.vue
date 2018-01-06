3fa6ad3d58b8cdfe718e9056603f50f0a0632abf
philosophy <totti_912@sina.com>
2018-01-06 16:56:45 +0800

    新品也改为新品推荐以及本店介绍两个部分

--- src/page/new/new.vue
+++ src/page/new/new.vue
@@ -1,13 +1,18 @@
 <template>
     <div class="new">
+        <logo></logo>
         <div class="swiper-wrapper" v-if="sliders.length>0">
             <divider>新品推荐</divider>
             <div class="swiper">
                 <swiper :list="sliders" auto loop style="width:100%;height:auto;" dots-class="custom-bottom" dots-position="center"></swiper>
             </div>
         </div>
-        <divider v-if="hots.length>0">热销商品</divider>
-        <div class="hot-wrapper" v-if="hots.length>0" ref="hotWrapper">
+        <div class="introduce">
+            <divider>本店简介</divider>
+            <p class="detail">Tian' Bakery主营点心、蛋糕、面包。店内环境温馨雅致，琳琅满目的面包、蛋糕，品种丰富多样，新鲜美味。每一个糕点都是上等选材经过细心烘烤而成的，口感细腻，处处散发着香醇的感觉，力求把健康、美味、营养、高品质的产品奉献给大家</p>
+        </div>
+        <!-- <div class="hot-wrapper" v-if="hots.length>0" ref="hotWrapper">
+            <divider v-if="hots.length>0">热销商品</divider>
             <grid :cols="2" >
                 <grid-item class="vux-1px-b" v-for="(product,i) in hots" :key="i">
                     <img :src="product.img">
@@ -16,12 +21,13 @@
                     </div>
                 </grid-item>
             </grid>
-        </div>
+        </div> -->
     </div>
 </template>
 
 <script type="text/ecmascript-6">
 import BScroll from 'better-scroll';
+import logo from '../../components/logo/logo';
 import { Swiper, Divider, Grid, GridItem, Flexbox, FlexboxItem } from 'vux';
 import Logger from 'chivy';
 const log = new Logger('cafe/new');
@@ -59,7 +65,8 @@ export default {
         Grid,
         GridItem,
         Flexbox,
-        FlexboxItem
+        FlexboxItem,
+        logo
     }
 };
 </script>
@@ -68,6 +75,14 @@ export default {
     .swiper-wrapper
         .swiper
             margin 5px 3px
+    .introduce
+        padding 5px
+        .detail
+            padding 0px 10px
+            text-indent 40px
+            font-size 16px
+            line-height 1.5
+
     .hot-wrapper
         position absolute
         top 260px
@@ -75,7 +90,8 @@ export default {
         left 0px
         overflow hidden
         .weui-grid
-            padding 10px 
+            padding 10px
+            border 1px solid red
         img
             width 100%
             height auto
