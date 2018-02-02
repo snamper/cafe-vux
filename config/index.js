c0d8ac83601a7788f963d38c55c0a59e9f5ac032
lizhe <lizhe@chinatsp.com>
2018-02-02 10:00:27 +0800

    修改为从后台获取数据

--- config/index.js
+++ config/index.js
@@ -31,7 +31,10 @@ module.exports = {
     autoOpenBrowser: false,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      "/shop": "http://www.tianbakery.store",
+      "/upload": "http://www.tianbakery.store"
+    },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
