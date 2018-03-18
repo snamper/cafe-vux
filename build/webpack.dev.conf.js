08faad4228f3fa3f0069415bd4cbf112dcc611bd
philosophy <totti_912@sina.com>
2018-03-18 17:19:56 +0800

    更改调试为跨域调试 增加微博登陆接口参数

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -10,7 +10,7 @@ const portfinder = require('portfinder')
 const express = require('express')
 const app = express()
 // 跨域访问 false表示不跨域
-const proxyType = false;
+const proxyType = true;
 
 // body-parse define
 const bodyParser = require('body-parser')
--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -59,7 +59,7 @@ export default {
     methods: {
         // 点击微博图标
         weibo() {
-            window.location.href = '/shop/member/show/ui/loginByOauth2.do';
+            window.location.href = '/shop/member/show/ui/loginByOauth2.do?accountType=weibo';
         },
         // 点击qq图标
         qq() {
