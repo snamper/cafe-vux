9bc12a8a0c0c79e6f7128e86cf00795883dc077d
lizhe <lizhe@chinatsp.com>
2018-06-13 18:01:38 +0800

    解决input框输入有输入法遮盖问题

--- src/pages/member/info.vue
+++ src/pages/member/info.vue
@@ -12,46 +12,102 @@
         icon="clear"
         placeholder="请输入姓名"
         required
-        @click-icon="name = ''"
-      />
+        @focus="close"
+        @click-icon="name = ''">
+      </van-field>
       <van-field
         v-model="birthday"
         label="生日"
         placeholder="请选择"
         required
-        @focus="selectbirthday"
-      />
+        @focus="selectbirthday">
+      </van-field>
+      <van-field
+        v-model="sex"
+        label="性别"
+        placeholder="请选择"
+        required
+        @focus="selectsex">
+      </van-field>
+      <van-field
+        v-model="mobile"
+        label="电话号码"
+        icon="clear"
+        placeholder="请输入电话号码"
+        required
+        @focus="close"
+        @click-icon="name = ''">
+      </van-field>
     </van-cell-group>
-    <van-datetime-picker v-if="show"
+    <van-datetime-picker v-if="show.birthday"
         v-model="currentDate"
         type="date"
-        :min-date="minDate"
-    />
+        @confirm="confirm"
+        @cancel="cancel">
+    </van-datetime-picker>
+    <van-dialog
+      v-model="show.sex"
+      show-cancel-button>
+      <van-picker 
+          show-toolbar
+          v-if="show.sex"
+          :columns="sexcolumns" 
+          @confirm="confirm"
+          @cancel="cancel">
+      </van-picker>
+    </van-dialog>
   </div>
 </template>
 
 <script type="text/ecmascript=6">
-import { Field, NavBar, CellGroup, DatetimePicker } from 'vant';
+import { Field, NavBar, CellGroup, DatetimePicker, Picker, Dialog } from 'vant';
 export default {
   data() {
     return {
-      show: false,
+      show: {
+        birthday: false,
+        sex: false
+      },
+      sex: '',
+      sexcolumns: ['请选择', '男', '女' ],
       name: '',
-      birthday: ''
+      birthday: '',
+      currentDate: '',
+      minDate: ''
     };
   },
   components: {
     [Field.name]: Field,
     [CellGroup.name]: CellGroup,
     [NavBar.name]: NavBar,
-    [DatetimePicker.name]: DatetimePicker
+    [DatetimePicker.name]: DatetimePicker,
+    [Picker.name]: Picker,
+    [Dialog.name]: Dialog
   },
   methods: {
+    close() {
+      this.show.birthday = false;
+      this.show.sex = false;
+    },
     back() {
 
     },
     selectbirthday() {
-      this.show = true;
+      this.show.birthday = true;
+      // 禁止手机调用输入法
+      document.activeElement.blur();
+    },
+    selectsex() {
+      this.show.sex = true;
+      document.activeElement.blur();
+    },
+    confirm() {
+      this.show.birthday = false;
+      this.show.sex = false;
+    },
+    cancel() {
+      this.show.birthday = false;
+      this.show.sex = false;
     }
   }
 };
